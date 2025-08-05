e7b8675a65b50009080d2d55e847d0a628083287
Tomas <tomas@studionumero.com>
2025-08-04 22:17:50 -0300

    Changed next config, added no jekyll to fix github pages site issue not showing css or js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true // needed if you use next/image
+  },
+  // Optional: basePath if deploying to a subfolder
+  basePath: '/englishbytom',
+}
+
+module.exports = nextConfig
