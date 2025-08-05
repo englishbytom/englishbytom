cdbb19807031ec03dfe73e1bee40c549cd57a58a
Tomas <tomas@studionumero.com>
2025-08-04 22:10:15 -0300

    Updates

---
+++ _next/static/chunks/app/cursos-grupales/page-308ee3f3668bc6f9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[256,264,305],{1149:(e,s,n)=>{Promise.resolve().then(n.bind(n,8596)),Promise.resolve().then(n.t.bind(n,6874,23))}},e=>{e.O(0,[639,548,596,441,964,358],()=>e(e.s=1149)),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/page-2051ff033285d8a4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{91:(e,s,n)=>{Promise.resolve().then(n.bind(n,8596)),Promise.resolve().then(n.t.bind(n,6874,23))}},e=>{e.O(0,[639,548,596,441,964,358],()=>e(e.s=91)),_N_E=e.O()}]);
