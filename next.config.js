1f1c2916ce7e8a129759189e5d7e8f9d99e86726
Tomas <tomas@studionumero.com>
2025-08-23 21:48:00 -0300

    Updated next config to be js instead of ts

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
-const nextConfig: NextConfig = {
+const nextConfig = {
   // output: 'export',
   pageExtensions: ["js", "jsx", "ts", "tsx"],
   images: {
